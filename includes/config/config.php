18e4713a41618f0286ab225f56a359d60a1a0dc6
j3ns09 <jens.ta@hotmail.com>
2025-06-02 23:11:00 +0200

    fix config issue

--- includes/config/config.php
+++ includes/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-    include_once('auths.php');
+    include_once($includesConfig . 'auths.php');
 
     try {
         $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_BASE . ";charset=utf8";
