de44a4d96960a306ce1f8c72abe29aeeab1a44f9
j3ns09 <jens.ta@hotmail.com>
2025-06-08 19:35:17 +0200

    deconnexion auto 15mn fix

--- includes/global/session.php
+++ includes/global/session.php
@@ -1,9 +1,20 @@
 <?php
 
-$inactive = 900; 
-ini_set('session.gc_maxlifetime', $inactive);
+$inactive = 900;
 
 session_start();
+
+// Check if last activity was set
+if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity'] > $inactive)) {
+    // 1800 seconds = 30 minutes
+    session_unset();
+    session_destroy();
+    header("Location: ../../login.php");
+    exit();
+}
+
+// Update last activity time stamp
+$_SESSION['last_activity'] = time();
 
 $root = $_SERVER['DOCUMENT_ROOT'] . "/";
 
