b24360b172dd32a0d94c3fb85ba9caa3b4cb2631
Nael Soualhia <nael92t@gmail.com>
2025-06-29 12:31:16 +0200

    ajout contact etc fonctionnels

--- contact.php
+++ contact.php
@@ -30,7 +30,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $mail->Host       = 'smtp.gmail.com';
             $mail->SMTPAuth   = true;
             $mail->Username   = 'myplaygroundcontact@gmail.com'; 
-            $mail->Password   = '**********'; 
+            $mail->Password   = 'bnrh iaek kbcw kjxd'; 
             $mail->SMTPSecure = 'tls';
             $mail->Port       = 587;
 
