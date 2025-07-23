fe2038ae0a952ba6fff1d50b24e56fcd9a2631f6
RxKujo <baazizray04@gmail.com>
2025-07-23 18:42:24 +0100

    desac import villes

--- processes/import_villes.php
+++ processes/import_villes.php
@@ -1,17 +1,17 @@
 <?php
-include_once '../includes/config/config.php';
+// include_once '../includes/config/config.php';
 
-$fichier = fopen('019HexaSmal.csv', 'r');
-fgetcsv($fichier, 0, ';');
+// $fichier = fopen('019HexaSmal.csv', 'r');
+// fgetcsv($fichier, 0, ';');
 
-while ($ligne = fgetcsv($fichier, 0, ';')) {
-    $insee = trim($ligne[0]);
-    $ville = trim($ligne[1]);
-    $cp = str_pad($ligne[2], 5, "0", STR_PAD_LEFT);
+// while ($ligne = fgetcsv($fichier, 0, ';')) {
+//     $insee = trim($ligne[0]);
+//     $ville = trim($ligne[1]);
+//     $cp = str_pad($ligne[2], 5, "0", STR_PAD_LEFT);
 
-    $stmt = $pdo->prepare("INSERT INTO villes_cp (ville, code_postal, code_insee) VALUES (?, ?, ?)");
-    $stmt->execute([$ville, $cp, $insee]);
-}
+//     $stmt = $pdo->prepare("INSERT INTO villes_cp (ville, code_postal, code_insee) VALUES (?, ?, ?)");
+//     $stmt->execute([$ville, $cp, $insee]);
+// }
 
-echo "Import terminé.";
+// echo "Import terminé.";
 ?>
