9ba00cdc2e5af594905fad0f100d2cb881655766
RxKujo <baazizray04@gmail.com>
2025-07-24 13:22:58 +0100

    import villes

--- seed/villes/import_villes.php
+++ seed/villes/import_villes.php
@@ -1,17 +1,23 @@
 <?php
-// include_once '../includes/config/config.php';
+// require_once "../includes/config/config.php";
 
-// $fichier = fopen('019HexaSmal.csv', 'r');
-// fgetcsv($fichier, 0, ';');
-
-// while ($ligne = fgetcsv($fichier, 0, ';')) {
-//     $insee = trim($ligne[0]);
-//     $ville = trim($ligne[1]);
-//     $cp = str_pad($ligne[2], 5, "0", STR_PAD_LEFT);
-
-//     $stmt = $pdo->prepare("INSERT INTO villes_cp (ville, code_postal, code_insee) VALUES (?, ?, ?)");
-//     $stmt->execute([$ville, $cp, $insee]);
+// $csv = fopen("cities.csv", "r");
+// if (!$csv) {
+//     die("Erreur d'ouverture du fichier CSV.");
 // }
 
-// echo "Import terminé.";
+// $pdo->exec("DELETE FROM villes_cp");
+
+// $ligne = 0;
+// while (($data = fgetcsv($csv, 1000, ",")) !== FALSE) {
+//     if ($ligne++ === 0) continue;
+
+//     list($csv_id, $code_postal, $nom_commune, $latitude, $longitude) = $data;
+
+//     $stmt = $pdo->prepare("INSERT INTO villes_cp (ville, code_postal, latitude, longitude) VALUES (?, ?, ?, ?)");
+//     $stmt->execute([$nom_commune, $code_postal, $latitude, $longitude]);
+// }
+
+// fclose($csv);
+// echo "Import terminé avec succès.";
 ?>
